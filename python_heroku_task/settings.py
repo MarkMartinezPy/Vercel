e36b0572fcd408e7c470a2dbc59ba7470bf73dee
MarkMartinezPy <160287995+MarkMartinezPy@users.noreply.github.com>
2024-04-15 14:22:13 -0400

    Update settings.py

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from os import environ
+
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv ()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get("llave_secreta")
+SECRET_KEY = 'django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
